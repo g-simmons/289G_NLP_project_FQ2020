8bd079d72a023416ae0386d4d6c3233d359dfeea
fangzhouli <fzli0805@gmail.com>
2020-11-11 02:11:53 -0800

    implemented attention and h_entities.

--- py/model.py
+++ py/model.py
@@ -1,6 +1,28 @@
+# -*- coding: utf-8 -*-
+"""The model configuration of Iterative Neural Network (INN).
+
+This module implements the INN model proposed by Cao (2019), Nested Relation
+Extraction with Iterative Neural Network.
+
+Example:
+
+        $ python model.py
+
+Authors:
+    Sammy Jia - https://github.com/sajia28
+    Fangzhou Li - https://github.com/fangzhouli
+
+TODO:
+    *
+
+"""
+
 # this file is dedicated toward holding the actual model
+from os import path
+import pickle
 import torch
 from torch import nn
+from torch.nn import functional as F
 from sklearn.model_selection import train_test_split
 
 # NOTES FROM PAPER
@@ -9,19 +31,39 @@ from sklearn.model_selection import train_test_split
 # The batch size is 8.
 
 
-class Model(nn.Module):
+class INNModel(nn.Module):
+    """INN model configuration.
+
+    Parameters:
+        vocab_dict (dict): The vocabulary for training, tokens to indices.
+        embedding_dim (int): The size of the embedding vector.
+        hidden_dim (int): The size of LSTM hidden vector, if bi-LSTM,
+            multilplied by 2
+
+    """
+
     def __init__(self, vocab_dict, embedding_dim, hidden_dim):
-        super(Model, self).__init__()
+        super().__init__()
+        self.vocab_dict = vocab_dict
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        # self.entity_labels = entity_labels
 
-        self.vocab_dict = vocab_dict        # vocabulary dictionary; converts tokens to indices
-        self.vocab_size = len(vocab_dict)   # vocabulary size
-        self.embedding_dim = embedding_dim  # size of embedding vector
-        self.hidden_dim = hidden_dim        # size of lstm hidden vector; blstm's is 2x
+        self.embedding_layer = nn.Embedding(
+            len(vocab_dict),
+            self.embedding_dim)
+        # For each hidden
+        self.attn_scores = nn.Linear(
+            in_features=self.hidden_dim * 2,
+            out_features=1)
+        self.blstm = nn.LSTM(
+            input_size=embedding_dim,
+            hidden_size=hidden_dim,
+            bidirectional=True,
+            num_layers=1)
 
-        self.embedding_layer = nn.Embedding(self.vocab_size, self.embedding_dim)
-        self.blstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=True, num_layers=1)
-
-    # accepts a sentence as input and returns a list of embedding vectors, one vector per token
+    # Accepts a sentence as input and returns a list of embedding vectors, one
+    #   vector per token.
     # i-th embedding vector corresponds to the i-th token
     def get_embeddings(self, sentence):
         token_list = sentence.split()
@@ -38,33 +80,71 @@ class Model(nn.Module):
 
         return self.embedding_layer(torch.LongTensor(index_list))
 
+    def get_h_entities(self, entities, blstm_out):
+        """Apply attention mechanism to entity representation.
+
+        Args:
+            entities (list of tuples::(str, (int,))): A list of pairs of an
+                entity label and indices of words.
+            blstm_out (torch.Tensor): The output hidden states of bi-LSTM.
+
+        Returns:
+            h_entities (torch.Tensor): The output hidden states of entity
+                representation layer.
+
+        """
+        h_entities = []
+        attn_scores_out = self.attn_scores(blstm_out)
+        for entity in entities:
+            h_entity = 0
+            attn_weights = F.softmax(attn_scores_out[list(entity[1])])
+            for i in range(len(attn_weights)):
+                h_entity += attn_weights[i] * blstm_out[list(entity[1])][i]
+            h_entities.append(h_entity)
+        h_entities = torch.cat(h_entities)
+        return h_entities
+
     # used to feed input into the model and have the model produce output
     # TO BE EDITED WHEN MORE LAYERS ARE ADDED
-    def forward(self, sentence):
+    # TODO
+    #   * Dataset instead a single sentence.
+    #   * Does input entities enough?
+    def forward(self, x):
+        sentence, entities = x
         embedded_sentence = self.get_embeddings(sentence)
-        # since bi-lstm's hidden vector is actually 2 concatenated vectors, it will be 2x as long (512)
-        blstm_out, _ = self.blstm(embedded_sentence.view(embedded_sentence.shape[0], 1, -1))
-
-        return blstm_out  # placeholder until we add more layers
+        # since bi-lstm's hidden vector is actually 2 concatenated vectors,
+        #   it will be 2x as long (512)
+        blstm_out, _ = self.blstm(embedded_sentence.view(
+            embedded_sentence.shape[0], 1, -1))
+        h_entities = self.get_h_entities(entities, blstm_out)
+        return h_entities  # placeholder until we add more layers
 
 
 def get_sentences(percent_test):
     with open('text_sentences.txt') as f:
         sentences = f.read().splitlines()
+    entities = pickle.load(
+        open(path.abspath(path.dirname(__file__)) + "/entities.pickle", 'rb'))
 
-    training_sentences, test_sentences = train_test_split(sentences, test_size=percent_test, random_state=0)
+    train_data, test_data = train_test_split(
+        list(zip(sentences, entities)), test_size=percent_test, random_state=0)
 
-    return training_sentences, test_sentences
+    return train_data, test_data
 
 
 def main():
-    training_sentences, test_sentences = get_sentences(0.2)
+    train_data, test_data = get_sentences(0.2)
 
     vocab_dict = eval(open('vocab_dict.txt', 'r').read())
     vector_dim = 256  # from page 6 of the paper
 
-    the_model = Model(vocab_dict, vector_dim, vector_dim)
-    print(the_model.forward(training_sentences[0]))  # example usage
+    model = INNModel(vocab_dict, vector_dim, vector_dim)
+
+    # example usage
+    print(train_data[0][0])
+    output = model.forward(train_data[0])
+    # print(len(train_data[0]))
+    # print(output.size())
     return 0
 
 
