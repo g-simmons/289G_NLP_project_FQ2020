7893ea5fb501158d9b4ed148b3c407ae5f164b19
Gabriel Simmons <simmons.gabe@gmail.com>
2020-12-12 00:11:02 -0800

    add config to wandb logging

--- py/train.py
+++ py/train.py
@@ -37,6 +37,8 @@ from config import (
     HIDDEN_STATE_CLAMP_VAL,
     XML_PATH,
     PREPPED_DATA_PATH,
+    LEARNING_RATE,
+    EXCLUDE_SAMPLES
 )
 
 BATCH_SIZE = 1
@@ -159,15 +161,32 @@ if __name__ == "__main__":
         hidden_state_clamp_val=HIDDEN_STATE_CLAMP_VAL,
     )
 
-    wandb_logger = WandbLogger(name='test-noclamp',project='nested-relation-extraction')
+    wandb_config = {
+        "batch_size": BATCH_SIZE,
+        "max_layers": MAX_LAYERS,
+        "learning_rate": LEARNING_RATE,
+        "cell_state_clamp_val": CELL_STATE_CLAMP_VAL,
+        "hidden_state_clamp_val": HIDDEN_STATE_CLAMP_VAL,
+        "vector_dim": VECTOR_DIM,
+        "word_embedding_dim" : WORD_EMBEDDING_DIM,
+        "hidden_dim": HIDDEN_STATE_CLAMP_VAL,
+        "relation_embedding_dim":RELATION_EMBEDDING_DIM,
+        "exclude_samples":EXCLUDE_SAMPLES
+    }
+
+    wandb_logger = WandbLogger(name='test',project='nested-relation-extraction',entity="ner")
     wandb_logger.watch(model,log='gradients',log_freq=1)
+    wandb_logger.log_hyperparams(wandb_config)
 
     trainer = pl.Trainer(
         gpus=GPUS,
         progress_bar_refresh_rate=1,
         automatic_optimization=False,
-        max_steps=50,
+        # overfit_batches=1,
+        # max_steps=50,
+        max_epochs = 3,
         # profiler="advanced",
+        val_check_interval=0.25,
         logger= wandb_logger
     )
 
