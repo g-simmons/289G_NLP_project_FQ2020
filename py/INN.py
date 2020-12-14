attempt to fix gradient flow

py/INN.py
@@ -76,6 +76,7 @@ class INNModel(pl.LightningModule):
 
         h_entities = []
         for sample in range(curr_batch_size):
+            print(entity_indices[sample])
             sample_entity_indices = [idx[idx >= 0] for idx in entity_indices[sample]]
             for idx in sample_entity_indices:
                 sample_attn_scores = attn_scores[sample][idx]
@@ -114,30 +115,36 @@ class INNModel(pl.LightningModule):
         ]
 
         # gets the hidden vector for each entity and stores them in H
-        _, candidate_splits = torch.unique_consecutive(A, return_counts=True)
         H = torch.randn(T.shape[0], BLSTM_OUT_DIM).detach().to(self.device)
         H = self.get_h_entities(
             entity_spans, blstm_out, token_splits, H, curr_batch_size, is_entity
         )
+        H.requires_grad_()
         C = (
             torch.zeros(T.shape[0], CELL_STATE_DIM)
             .clone()
             .detach()
-            .requires_grad_()
             .to(self.device)
         )
+        C.requires_grad_()
 
-        predictions = [PRED_TRUE if is_entity[i] == 1 else PRED_FALSE for i in range(T.shape[0])]
-        predictions = torch.stack(predictions).requires_grad_()
+        predictions = [PRED_TRUE if is_entity[i] == 1 else PRED_FALSE for i in range(0,T.shape[0])]
+        predictions = torch.stack(predictions).to(self.device)
+        predictions.requires_grad_()
 
         for layer in torch.unique(L):
             if layer > 0:
                 predictions = predictions.clone()
                 parent_mask = self._get_parent_mask(L, layer, element_names, is_entity)
+                args_predicted = torch.all(torch.ge(predictions[S][:,:,1], 0.5),dim=1)
+                parent_mask = torch.logical_and(parent_mask,args_predicted)
                 element_embeddings = self.element_embeddings(element_names[parent_mask])
-                h, c = self.cell.forward(H, C, element_embeddings, S[parent_mask, :])
-                H = H.clone()
-                C = C.clone()
+                s = S[parent_mask, :]
+                v = H[S]
+                v = v.flatten(start_dim=1)
+                cell_states = C[S]
+                cell_states = cell_states.flatten(start_dim=1)
+                h, c = self.cell.forward(v, c, element_embeddings, s)
                 H[parent_mask, :] = h
                 C[parent_mask, :] = c
                 logits = self.output_linear(H[parent_mask, :])

py/daglstmcell.py
@@ -51,21 +51,13 @@ class DAGLSTMCell(pl.LightningModule):
         assert cell_states.shape[1] == self.hidden_dim
         assert fj.shape[1] == self.hidden_dim
 
-    def _get_concatenated_cell_states(self, C, S):
-        cell_states = torch.stack([torch.flatten(C[idx]) for idx in S])
-        return cell_states
+    def forward(self, hidden_vectors, cell_states, element_embeddings, S):
+        # if VAL_DIMS:
+        #     H, C, element_embeddings, S, n_entities, n_args = self._validate_inputs(
+        #         H, C, element_embeddings, S
+        #     )
 
-    def _get_concatenated_hidden_states(self, H, S):
-        v = torch.stack([torch.flatten(H[idx]) for idx in S])
-        return v
-
-    def forward(self, H, C, element_embeddings, S):
-        if VAL_DIMS:
-            H, C, element_embeddings, S, n_entities, n_args = self._validate_inputs(
-                H, C, element_embeddings, S
-            )
-        cell_states = self._get_concatenated_cell_states(C, S)
-        v = self._get_concatenated_hidden_states(H, S)
+        v = hidden_vectors
 
         ioc_hat = self.W_ioc_hat(element_embeddings)
         ioc_hat += self.U_ioc_hat(v)
@@ -74,14 +66,10 @@ class DAGLSTMCell(pl.LightningModule):
         i, o, c_hat = torch.chunk(ioc_hat, 3, dim=1)
         i, o, c_hat = torch.sigmoid(i), torch.sigmoid(o), torch.tanh(c_hat)
 
-        fj = self.W_fs(element_embeddings)
+        fj = torch.sigmoid(self.W_fs(element_embeddings) + self.U_fs(v) + self.b_fs)
 
-        vbc = v.repeat(1, self.max_inputs)
-        fj += self.U_fs(v)
-        fj += self.b_fs
-
-        fj = torch.sigmoid(fj)
         fj = fj.reshape(-1, fj.shape[1] // 2)
+
         cell_states = cell_states.reshape(-1, cell_states.shape[1] // 2)
         if VAL_DIMS:
             self._val_fj_cell_states(fj, cell_states, n_args)